05792da13604e09ec55d54052ff87b703c2e1a2a
SaabiriinAbdi <Saabiriin46@gmail.com>
2025-04-13 17:42:46 -0500

    Saabiriin Pushed Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router';
 import Navbar from './components/Navbar';
-import Home from './pages/Home'
+import Home from './pages/Home';
+import FeaturePage from './pages/Overview';
 import Learnmore from './pages/Learnmore';
 import Footer from './components/Footer';
 import './styles/App.css'
@@ -14,10 +15,13 @@ function App() {
       <div className='App'>
         <Router>
           <Navbar/>
+          <div id ="wrap-content">
           <Routes>
             <Route path='/' exact element={<Home/>}/>
+            <Route path="/overview" element={<FeaturePage />} />
             <Route path='/learnmore' exact element={<Learnmore/>}/>
           </Routes>
+          </div>
           <Footer/>
         </Router>
       </div>
