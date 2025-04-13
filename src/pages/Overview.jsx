919554a1e3adcf8625481f9be0e5c40a3f91eb51
PT-mon <kyrmendo2@gmail.com>
2025-04-13 18:19:24 -0500

    Pekdey's Skeleton for Overview Page

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
 
-function FeaturePage() {
+function Overview() {
   return (
+    <>
+    <title> This is the Overview page!</title>
+
     <div>
+      <h1> Overview </h1>
+      <p>Welcome to the Overview Page!</p> 
+      <p>Here you will find a brief introduction about the types website accessibility
+         as well as sneak peaks into what to expect for the other pages.
+      </p>
+
+      <div className ="See">
+      <h>EYE</h>
+      <p>MY EYES</p>
+      </div>
+
+      <div className ="Hear">
+      <h>EAR</h>
+      <p>MY EARS</p>
+      </div>
+
+      <div className ="Typing">
+      <h>FINGER</h>
+      <p>MY FINGIES</p>
+      </div>
+
       
+      <h>Showcase</h>
+      <p>showcase context</p>
+      <h>Quiz</h>
+      <p>quiz context</p>
+      <h>Learn More</h>
+      <p>learn more context</p>
     </div>
+    </>
   )
 }
 
-export default FeaturePage
+export default Overview
