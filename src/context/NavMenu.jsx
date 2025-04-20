0dac7a3abc331e76b351c36505a520d468ca32fb
jjroberson8 <jjroberson8@gmail.com>
2025-04-20 17:08:16 -0500

    Fixed css to Quiz page. Added contact us page

--- src/context/NavMenu.jsx
+++ src/context/NavMenu.jsx
@@ -7,12 +7,11 @@ import { faHouse, faBookOpen, faClipboardQuestion,faCircleInfo, faStar} from '@f
 {/* Nav Links for the navbar because it is used in multiple places*/}
 function NavMenu() {
   return (
-    
     <>
     <ul className='nav-menu'>
         <li><Link to='/'> <FontAwesomeIcon icon={faHouse} alt='Home icon'/> Home </Link></li>
         <li><Link to='/overview'><FontAwesomeIcon icon={faBookOpen} alt='Book icon'/> Overview </Link></li>
-        <li><Link to='/showcase'><FontAwesomeIcon icon={faStar} /> Showcase </Link></li>
+        <li><Link to='/showcase'><FontAwesomeIcon icon={faStar} alt='Star icon'/> Showcase </Link></li>
         <li><Link to='/quiz'><FontAwesomeIcon icon={faClipboardQuestion} alt='Clipboard icon'/> Quiz Yourself</Link></li>
         <li><Link to='/learnmore'><FontAwesomeIcon icon={faCircleInfo} alt='Info icon'/> Learn More </Link></li>
     </ul>
