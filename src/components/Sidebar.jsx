41b2551c606ea04d9d0c8c8d36ff071b5d4cfebc
jjroberson8 <jjroberson8@gmail.com>
2025-04-24 19:39:39 -0500

    FOR THE LOVE OF GODDDDDDDDD

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ function Sidebar ({ items })  {
       <ul>
         {items.map((item, index) => ( //maps through the items array and creates a list item for each element
           <li key={index}> {/* key is a unique identifier for each list item, using index as a fallback */}
-            <a href={`${location.pathname}#${item.href}`}>{item.label}</a> {/* href is the link to the section and label is the text that will be displayed */}
+            <a href={`${location.pathname.slice(1)}#${item.href}`}>{item.label}</a> {/* href is the link to the section and label is the text that will be displayed */}
           </li>
         ))}
       </ul>
